390f5a11b8cfbf5d05944a49862b000c2ac0d2c6
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-12-01 13:06:28 +0100

    disable type checks

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    typescript: {
+        // source: https://nextjs.org/docs/app/api-reference/next-config-js/typescript
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
