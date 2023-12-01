9ad932c5da1ca278fd5280be18dd3a685a5814e6
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-12-01 16:02:18 +0100

    seed customers & invoices

--- scripts/seed.js
+++ scripts/seed.js
@@ -7,7 +7,10 @@ const {
   users,
 } = require('../app/lib/placeholder-data.js');
 const bcrypt = require('bcrypt');
-const revenueModel = require("../app/models/revenue");
+const revenueModel = require("../app/models/revenue.js");
+const invoicesModel = require("../app/models/invoices.js");
+const customerModel = require('../app/models/customer.js');
+
 
 main().catch(err => console.log(err));
 
@@ -15,9 +18,9 @@ async function main() {
     await mongoose.connect(process.env.MONGODB_URI);
     // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   
-//   await seedUsers();
-//   await seedCustomers();
-//   await seedInvoices();
+    // await seedUsers();
+    await seedCustomers();
+    await seedInvoices();
     await seedRevenue();
 
     mongoose.connection.close()
@@ -74,36 +77,16 @@ async function seedUsers() {
 
 async function seedInvoices() {
   try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
-    // Create the "invoices" table if it doesn't exist
-    const createTable = await client.sql`
-    CREATE TABLE IF NOT EXISTS invoices (
-    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    customer_id UUID NOT NULL,
-    amount INT NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    date DATE NOT NULL
-  );
-`;
-
-    console.log(`Created "invoices" table`);
-
-    // Insert data into the "invoices" table
-    const insertedInvoices = await Promise.all(
-      invoices.map(
-        (invoice) => client.sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-      ),
-    );
+    // clear collection before seeding
+    const clearCollection = await invoicesModel.deleteMany({})
+    console.log(`removed ${clearCollection.deletedCount} invoices`)
+    
+    // Insert data into the "invoices" collection
+    const insertedInvoices = await invoicesModel.insertMany(invoices)
 
     console.log(`Seeded ${insertedInvoices.length} invoices`);
 
     return {
-      createTable,
       invoices: insertedInvoices,
     };
   } catch (error) {
@@ -114,35 +97,16 @@ async function seedInvoices() {
 
 async function seedCustomers() {
   try {
-    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // clear collection before seeding
+    const clearCollection = await invoicesModel.deleteMany({})
+    console.log(`removed ${clearCollection.deletedCount} customers`)
 
-    // Create the "customers" table if it doesn't exist
-    const createTable = await client.sql`
-      CREATE TABLE IF NOT EXISTS customers (
-        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        image_url VARCHAR(255) NOT NULL
-      );
-    `;
-
-    console.log(`Created "customers" table`);
-
-    // Insert data into the "customers" table
-    const insertedCustomers = await Promise.all(
-      customers.map(
-        (customer) => client.sql`
-        INSERT INTO customers (id, name, email, image_url)
-        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-        ON CONFLICT (id) DO NOTHING;
-      `,
-      ),
-    );
+    // Insert data into the "customers" collection
+    const insertedCustomers = await customerModel.insertMany(customers)
 
     console.log(`Seeded ${insertedCustomers.length} customers`);
 
     return {
-      createTable,
       customers: insertedCustomers,
     };
   } catch (error) {
@@ -155,11 +119,11 @@ async function seedRevenue() {
   try {
     // clear collection before seeding
     const clearCollection = await revenueModel.deleteMany({})
-    console.log(`removed ${clearCollection.deletedCount} revenue`)
+    console.log(`removed ${clearCollection.deletedCount} revenues`)
 
     // Insert data into the "revenue" collection
     const insertedRevenue = await revenueModel.insertMany(revenue)
-    console.log(`Seeded ${insertedRevenue.length} revenue`);
+    console.log(`Seeded ${insertedRevenue.length} revenues`);
 
     return {
       revenue: insertedRevenue,
