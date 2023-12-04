a5da26d0f5b30caa6d9292bdd0132c18dd0f3277
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-12-04 20:14:18 +0100

    remove test error from deleteInvoice()

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,7 +69,6 @@ export async function updateInvoice(id, formData) {
 
 
 export async function deleteInvoice(id) {
-    throw new Error('Failed to Delete Invoice');
     try {
         console.log(`deleteInvoice action for id ${id}`)
     
