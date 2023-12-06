17490f74751ebc2ba1073876f361f071484bee63
Samuel Hartmann <hartmann.samuel@gmx.de>
2023-12-06 14:49:53 +0100

    metadata in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import '@/app/ui/global.css'
 import { inter } from '@/app/ui/fonts'
 
+export const metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, build with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
